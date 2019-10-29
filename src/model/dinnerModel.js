736d1b1b0ec74e391b896f63bf9c0bb009a1ad46
linus johannisson <mrjohannisson@hotmail.com>
2019-10-29 16:27:34 +0100

    lab1_proceduralv2

--- src/model/dinnerModel.js
+++ src/model/dinnerModel.js
@@ -39,17 +39,22 @@ class DinnerModel {
   //Returns all ingredients for all the dishes on the menu.
   getAllIngredients() {
     //TODO Lab 0
-	let y = {};
-	for(let x of menu.dishes){
+	let y = [];
+	for(let x of this.dinner.menu){
 		for(let z of x.ingredients){
-			y += z;
+			y.push(z);
 		}
 	}
 	return y;
 	}
 	//Returns the total price of the menu (all the ingredients multiplied by number of guests).
 	getTotalMenuPrice() {
-	//TODO Lab 0
+		let z = 0;
+		x = this.getAllIngredients;
+		for( y of x.price){
+			z = y*this.dinner.guests;
+		}
+		return z;
 	}
 
 	//Adds the passed dish to the menu. If the dish of that type already exists on the menu
